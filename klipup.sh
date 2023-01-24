53a352d90073daba18083933b6088317eeda1c44
ShanDestromp <Shan.Destromp@gmail.com>
2023-01-24 10:29:23 -0700

    Push to backup existing

--- klipup.sh
+++ klipup.sh
@@ -2,9 +2,8 @@
 
 VER="$(echo $1 | tr '[A-Z]' '[a-z]')" #lowercase
 
-# THIS PATH MIGHT CHANGE
 # Recommend using /dev/serial/by-id/YOUR_DEVICE_HERE
-DEV=/dev/ttyACM0 #THIS PATH MIGHT CHANGE
+DEV=/dev/serial/by-id/usb-Prusa_Research__prusa3d.com__Original_Prusa_i3_MK3_CZPX3118X004XK76866-if00
 
 if [ "$VER" != "prusa" ] && [ "$VER" != "koc" ]
 then
@@ -22,13 +21,12 @@ fi
 
 git pull
 
-./scripts/install-octopi.sh
+#./scripts/install-octopi.sh
 make menuconfig
 make
 sudo service klipper stop
 
 make flash FLASH_DEVICE=${DEV}
-
 
 rm $HOME/klipper
 ln -sf `pwd` $HOME/klipper
