efaf5b9cd2920f5ec410c6ba2a634e3a35c7e6fd
ShanDestromp <Shan.Destromp@gmail.com>
2024-04-20 11:31:07 -0400

    Input Shaping and misc cleanup

--- klipup.sh
+++ klipup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-VER="$(echo $1 | tr '[A-Z]' '[a-z]')" #lowercase
+SCRIPT=$(readlink -f $0)
+SCPATH=`dirname $SCRIPT`
 
 # Recommend using /dev/serial/by-id/YOUR_DEVICE_HERE
 DEV=/dev/serial/by-id/usb-Prusa_Research__prusa3d.com__Original_Prusa_i3_MK3_CZPX3118X004XK76866-if00
@@ -15,5 +16,8 @@ make
 sudo service klipper stop
 
 make flash FLASH_DEVICE=${DEV}
+
+# Uses local copy of PrusaOwners tram_z script and makes it available to the upstream ver of Klipper
+ln -s $SCPATH/misc/tram_z.py ~/klipper/klippy/extras/tram_z.py
 
 sudo service klipper restart
